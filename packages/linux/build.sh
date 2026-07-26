c9313d9a07ebc7bd121ca136aaa549bcb02adf74
Nixxy-lv <dimeodiego9@gmail.com>
2026-07-26 20:35:56 +0200

    v0.1.2: cleaned Kernel build script and config, added /etc/os-release and changed root user's name to Kore

--- packages/linux/build.sh
+++ packages/linux/build.sh
@@ -12,27 +12,17 @@ if [ ! -d linux-$KV ]; then
 fi
 
 cd linux-$KV
+
 cp ../config/x86_64.config .config
 
-export KCFLAGS="-std=gnu17"
+export KCFLAGS="-std=gnu17 -Wno-error=implicit-function-declaration"
 export HOSTCFLAGS="-std=gnu17"
 
 make olddefconfig > $ROOT/out/logs/kernel-config.log 2>&1
 
-## Force flags
-echo "Forzing Initramfs and GZIP support..."
-echo "CONFIG_BLK_DEV_INITRD=y" >> .config
-echo "CONFIG_BLK_DEV_RAM=y" >> .config
-echo "CONFIG_RD_GZIP=y" >> .config
-echo "CONFIG_DECOMPRESS_GZIP=y" >> .config
-
-echo "CONFIG_ACPI_SYSFS_POWER=y" >> .config
-# To validate dependencies, no overwrite
-make oldconfig < /dev/null > /dev/null 2>&1
-
 echo "Compiling kernel... grab a coffee"
 
-KCFLAGS="-std=gnu17 -Wno-error=implicit-function-declaration" HOSTCFLAGS="-std=gnu17" make KCFLAGS="-std=gnu17" HOSTCFLAGS="-std=gnu17" -j$(nproc) bzImage > $ROOT/out/logs/kernel-build.log 2>&1
+make -j$(nproc) bzImage > $ROOT/out/logs/kernel-build.log 2>&1
 
 cp arch/x86/boot/bzImage $ROOT/out/kernel/bzImage
 echo "OK -> out/kernel/bzImage $(du -h $ROOT/out/kernel/bzImage | cut -f1)"
---
+++ rootfs/root/.profile
@@ -0,0 +1,8 @@
+#######################################
+## This is the RC config for /bin/sh ##
+#######################################
+
+alias ff="fastfetch"
+alias la="ls -A"
+alias lh="ls -h"
+alias clr="clear"
